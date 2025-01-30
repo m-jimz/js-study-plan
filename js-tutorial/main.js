de056ba69dbca94ab70aec49fbc2f61ae4a2d40d
Marco <marcjimenez790@gmail.com>
2025-01-30 16:23:25 -0600

    Create Notes and Code from Tutorial

---
+++ js-tutorial/main.js
@@ -0,0 +1,38 @@
+
+// variable: named storage for data, to create one use "let"
+
+// message = "Hello"; // store the string "Hello" in the variable named message
+
+// alert(message); // shows the variable content 
+
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+// var and let are almost the same 
+
+message = 'Hello';
+
+message = 'World!'; // value changed 
+
+console.log(message);
+
+let hell0 = 'Hello world!';
+
+// copy 'Hello world' from hello into message
+
+let userName;
+let test123;
+
+let $ = 1; // declared a varaible with the name "$" 
+let _ = 2; // and now a variable with the name "_"
+
+console.log($ + _);
+
+// cannot start with a digit 
+// hyphens '-' aren't allowed in the name 
+
+const myBirthday = '18.04.1982';
+
+// const or constants can be reassigned 
+
---
+++ js-tutorial/notes.md
@@ -0,0 +1,19 @@
+## comments
+- create comments by using //
+- use ctrl + / to create comments
+- nested are not supported 
+
+
+
+## "use strict" 
+- top of your scripts it will be ignored 
+- no way to cancel use strict
+- shift + enter for a new line 
+
+
+
+## variables to store data 
+- let - modern variable declaration
+- var - old-school variable declartion
+- const is like let, but the value can not be changed 
+- remember to put semicolons between statement despite being separated by newlines 
